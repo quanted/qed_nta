208aae057d5f8496f08428383eac994b025b6405
Tom Purucker <purucker.tom@gmail.com>
2019-02-22 17:03:42 -0500

    update heads

--- wsgi_local.py
+++ wsgi_local.py
@@ -7,14 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
-
-import django.core.handlers.wsgi
 from django.core.wsgi import get_wsgi_application
+from temp_config.set_environment import DeployEnv
 
 print('wsgi_local.py')
-# Settings.py declaration
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+## Settings.py declaration
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-# entry point
-app = django.core.handlers.wsgi.WSGIHandler()
+## entry point
+#app = django.core.handlers.wsgi.WSGIHandler()
+#application = get_wsgi_application()
+
+# Determine env vars to use:
+runtime_env = DeployEnv()
+runtime_env.load_deployment_environment()
+
+if os.environ.get('DJANGO_SETTINGS_FILE'):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ.get('DJANGO_SETTINGS_FILE'))
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_docker")
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_docker")
+
 application = get_wsgi_application()
