ef26c906394d9957a88390eb9dee155171487783
Jeff Minucci <jminucci2@gmail.com>
2019-11-04 13:40:43 -0500

    clean up urls.py

--- urls.py
+++ urls.py
@@ -1,18 +1,9 @@
 # Django 2.0 url import
 from django.urls import include, path
 
-#regular expressions
-# the r in r'^cts/index.html$' indicates that what is inside the quotes is a regular expression
-# the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
-# the $ in r'^cts/index.html$' indicates that we are looking to extend end the mathing part exactly here
-
 print('qed.urls')
 #appends to the list of url patterns to check against
 urlpatterns = [
-    ##django 1.X
-	#url(r'^', include('splash_app.urls')),
-    #url(r'^pram', include('pram_app.urls')),
-    #django 2.X
     path('', include('splash_app.urls')),
     path('nta/', include('nta_app.urls')),
 ]
